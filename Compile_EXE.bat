7723ee2cffce09cc50dd9cbfbc53206d6e9ae4f7
derwookie <51955045+derwookie@users.noreply.github.com>
2022-01-05 12:41:42 +0100

    v0.1-beta
    First beta-Release

--- Compile_EXE.bat
+++ Compile_EXE.bat
@@ -3,14 +3,14 @@ rmdir /s /q dist
 del shrink.spec
 
 REM Build the new .exe-file out of the original script
-pyinstaller -c -F -i icon.ico shrink.py
+pyinstaller -c -F -i src/icon.ico src/shrink.py
 
 :: Clear the build-directories
-rmdir /s /q __pycache__
+rmdir /s /q src\__pycache__
 rmdir /s /q build
 
 :: Copy the Icon into the final directory
-xcopy /v /y icon.ico .\dist
+xcopy /v /y src\icon.ico .\dist
 
-REM ".exe con be found in ./dist"
+REM ".exe con be found in .\dist"
 pause
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # VBoxShrinker
 A little GUI-Application written in Python with Tkinter to shrink existing VirtualBox vdi-files if they are shrinkable
+
